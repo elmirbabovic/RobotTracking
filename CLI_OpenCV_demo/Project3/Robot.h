12f9979daa32bfcc8a1b578675e68a832c1524a7
Adil Joldic <adil@edu.fit.ba>
2018-11-10 23:33:57 +0100

    update

---
+++ CLI_OpenCV_demo/Project3/Robot.h
@@ -0,0 +1,68 @@
+#pragma once
+#include "MyMath.h"
+#include "SlidingPath.h"
+
+class Robot
+{
+	SlidingPath* historijaPozicija = new SlidingPath(30);
+
+	int id;
+public:
+	bool isRemoved = false;
+	float* ugaoPravcaKretanja = nullptr;
+
+	float Udaljenost_OdTacke(cv::Point p)
+	{
+		MotionStep* frame_point = this->GetPozicijaNajnovija();
+		if (frame_point == nullptr)
+			return INT_MAX;
+
+		return Udaljenost_DvijeTacke(frame_point->item, p);
+	}
+
+	Robot(int id, int framePozicija, int x, int y)
+	{
+		this->id = id;
+		DodajPoziciju(framePozicija, x, y);
+	}
+
+	int GetId()
+	{
+		return id;
+	}
+
+	MotionStep* GetPozicijaNajnovija()
+	{
+		int velicina = historijaPozicija->getVelicina();
+		if (velicina == 0)
+			return nullptr;
+
+		MotionStep* frame_point = historijaPozicija->GetNajnovija();
+		return frame_point;
+	}
+
+	void DodajPoziciju(int framePozicija, int x2, int y2)
+	{
+		cv::Point p2 = cv::Point(x2, y2);
+		if (historijaPozicija->getVelicina() > 2)
+		{
+			MotionStep* mPoint1 = historijaPozicija->getOlderVersion(1);
+
+			if (mPoint1 != nullptr)
+			{
+				cv::Point p1 = mPoint1->item;
+				int Dx = DeltaX(p1, p2);
+				int Dy = DeltaY(p1, p2);
+				if (abs(Dy) > 2 || abs(Dx) > 2)
+				{
+					if (ugaoPravcaKretanja == nullptr)
+						ugaoPravcaKretanja = new float;
+
+					*ugaoPravcaKretanja = IzracunajUgao(Dx, Dy);
+				}
+			}
+
+		}
+		historijaPozicija->dodaj(framePozicija, p2);
+	}
+};
