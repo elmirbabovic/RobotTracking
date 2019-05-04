ca434a0e7318e6923c5c846e8f2061712ad126a5
Elmir Babovic <babovi.elmir@bih.net.ba>
2019-05-05 00:55:43 +0200

    Attempt to predict by SH algorythm

--- RobotControl_1.1/RobotControl_1/Robot.h
+++ RobotControl_1.1/RobotControl_1/Robot.h
@@ -31,7 +31,7 @@ enum PointType
 
 class Robot
 {
-
+	int brojac=0;//only for logic debugging
 	MotionHistory* motionHistoryFront = new MotionHistory(30);
 	MotionHistory* motionHistoryRear = new MotionHistory(30);
 	
@@ -66,14 +66,15 @@ public:
 
 	Lista<cv::Point>* PredictPath()
 	{
+		//smoothing factors alpha and beta
+		float alpha = 0.3;
+		float beta = 0.5;
+
 		ListaPov<cv::Point> *result = new ListaPov<cv::Point>;
 
 		int velicina = motionHistoryFront->Count();
-		if (velicina == 0)
+		if (velicina < 30)
 			return result;
-
-		
-
 
 		for(int i=velicina-1; i>=0;i--)
 		{
@@ -82,9 +83,93 @@ public:
 			result->Add(cv::Point(p.x , p.y ));
 		}
 
-		//ovdje ide for za predikciju po double exponential smoothing za jos 10 tacaka
-				
+	
 
+		//ovdje ide for za predikciju po double exponential smoothing za jos 10 tacaka
+		const int predictionSize = 40;
+		float sX[predictionSize];
+		float bX[predictionSize];
+		float yX[predictionSize+1];
+		float sY[predictionSize];
+		float bY[predictionSize];
+		float yY[predictionSize+1];
+		float datX[predictionSize];
+		float datY[predictionSize];
+
+		cv::Point predictionResult[predictionSize];
+
+		datX[0] = alpha * result->get(0).x + result->get(0).x;
+		datY[0] = alpha * result->get(0).y + result->get(0).y;
+
+		for (int i = 1; i < 30; i++)
+		{
+			datX[i] = alpha * result->get(i).x + (1-alpha)*result->get(i-1).x;
+			datY[i] = alpha * result->get(i).y + (1 - alpha)*result->get(i - 1).y;
+		}
+
+
+		sX[0] = result->get(0).x;
+		sY[0] = result->get(0).y;
+		yX[0] = datX[0];
+		yY[0] = datY[0];
+		sX[1] = datX[1];
+		sY[1] = datY[1];
+		bX[0] = 1;//data[1]-data[0];
+		bY[0] = 1;//data[1]-data[0];
+		bX[1] = result->get(1).x - result->get(0).x;
+		bY[1] = result->get(1).y - result->get(0).y;
+
+		int i = 1;
+		yX[0] = sX[0] + bX[0];
+		yY[0] = sY[0] + bY[0];
+		yX[1] = sX[1] + 2 * bX[1];
+		yY[1] = sY[1] + 2 * bY[1];
+
+		for (i = 2; i < 30; i++) {
+			sX[i] = alpha * datX[i] + (1 - alpha) * (sX[i - 1] + bX[i - 1]);
+			sY[i] = alpha * datY[i] + (1 - alpha) * (sY[i - 1] + bY[i - 1]);
+			bX[i] = (beta * (sX[i] - sX[i - 1])) + (1 - beta) * bX[i - 1];
+			bY[i] = (beta * (sY[i] - sY[i - 1])) + (1 - beta) * bY[i - 1];
+			yX[i + 1] = sX[i] + i * bX[i];
+			yY[i + 1] = sY[i] + i * bY[i];
+		}
+
+		for (i=29; i < 40; i++) {
+			sX[i] = alpha * sX[i-1] + (1 - alpha) * (sX[i - 1] + bX[i - 1]);
+			sY[i] = alpha * sY[i-1] + (1 - alpha) * (sY[i - 1] + bY[i - 1]);
+			bX[i] = (beta * (sX[i] - sX[i - 1])) + (1 - beta) * bX[i - 1];
+			bY[i] = (beta * (sY[i] - sY[i - 1])) + (1 - beta) * bY[i - 1];
+			yX[i + 1] = sX[i] + i * bX[i];
+			yY[i + 1] = sY[i] + i * bY[i];
+		}
+
+		for (int j = 0; j < 10; j++) {
+			yX[j + 30] = sX[j + 29] + (j + 30)*bX[j + 29];
+			yY[j + 30] = sY[j + 29] + (j + 30)*bY[j + 29];
+		}
+
+		for(int a=0;a<40;a++)
+		{
+			predictionResult[a].x = yX[a];
+			predictionResult[a].y = yY[a];
+		}
+
+		for (int t = 0; t < predictionSize; t++)
+		{
+			result->Add(predictionResult[t]);
+		}
+		brojac++;
+		if (brojac%50==0)
+		{
+			ofstream f("result.txt", ios::app);
+			f << "--------------------------" << endl;
+			f << "Novi ispis: " << brojac <<endl;
+			for (int j = 0; j < 40; ++j)
+			{
+				f << result->get(j).x << "," << result->get(j).y << endl;
+			}
+			f.close();
+		}
 		return result;
 		
 	}
